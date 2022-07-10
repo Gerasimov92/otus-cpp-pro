d0d256be2b5e0ebaf478d6276f00ed2ea80951ee
Ivan Gerasimov <aka.montoya@gmail.com>
2022-07-10 22:20:16 +0300

    Add optional filename postfix

--- homework7/SaveToFileVisitor.h
+++ homework7/SaveToFileVisitor.h
@@ -2,13 +2,16 @@
 
 #include "IVisitor.h"
 #include <fstream>
+#include <string>
 
 class SaveToFileVisitor : public IVisitor
 {
 public:
+    SaveToFileVisitor(const std::string &postfix = "");
     void visitCommand(Command &cmd) override;
     void visitCommandBulk(CommandBulk &bulk) override;
 
 private:
-    std::ofstream out;
+    std::ofstream m_out;
+    std::string m_postfix;
 };
